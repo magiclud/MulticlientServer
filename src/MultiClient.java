4dc5ce34a8e58e0841e0657d8ba633dff1e05d3f
magiclud <you@example.com>
2014-04-22 19:31:51 +0200

    pierwszy

---
+++ src/MultiClient.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+
+public class MultiClient implements Runnable {
+
+	private static Socket clientSocket = null;
+	private static PrintStream os = null;	//outout stream
+	private static DataInputStream is = null; //input stream
+	
+	private static BufferedReader inputLine = null; 
+	private static boolean closed = false;
+	
+	public static void mian(String[] args){
+		//default port 
+		int portNumber = 3333;
+		//default host 
+		String host = "localhost";
+		
+		if(args.length < 2){
+			System.out.println("Usage: host: "+ host+ ", portNumber="+ portNumber);
+		}
+		else{
+			host = args[0];
+			portNumber = Integer.valueOf(args[1]).intValue();
+		}
+		
+		//open a socket on a given host and port, open input and output streams
+		try{
+			clientSocket = new Socket(host, portNumber);
+			inputLine = new BufferedReader(new InputStreamReader(System.in));
+			os = new PrintStream(clientSocket.getOutputStream());
+			is = new DataInputStream(clientSocket.getInputStream());
+		////}catch(UnknowHostException e){
+		//	System.err.println("Don't know about host "+ host);
+		}catch(IOException e){
+			System.err.println("Couldn't get I/O for the connection to the host " + host);
+		}
+		//open a connection to on the portNumber and write some data
+		if(clientSocket != null && os != null && is != null){
+			try {
+				//create thread to read wrom the server
+				new Thread(new MultiClient()).start();
+				while(!closed){
+					os.println(inputLine.readLine().trim());
+				}
+				//close the outputStream, close the input stream, close the socket
+				os.close();
+				is.close();
+				clientSocket.close();
+			}catch(IOException e){
+				System.err.println("IOException: " + e);
+			}
+		}
+	}
+	//create a thread to read from the server
+	@Override
+	public void run() {
+		
+		String responseLne;
+		try{
+			while((responseLne = is.readLine()) != null){
+				System.out.println(responseLne);
+				if(responseLne.indexOf("*** Cliet leave the connection") != -1){
+					break;
+				}
+				if(responseLne.startsWith("from")){
+					System.out.println("dziala");
+				}
+			}
+			closed = true;
+		}
+catch(IOException e){
+	System.err.println("IOException: " + e);
+}		
+	}
+
+}
---
+++ src/MultiServer.java
@@ -0,0 +1,132 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class MultiServer {
+	
+	private static ServerSocket serverSocket = null;
+	private static Socket clientSocket = null;
+	
+	private static int maxClientsCount = 2;
+	
+	//array with threads of each client
+	private static ClientThread[] threads = new ClientThread[maxClientsCount];
+	
+	public MultiServer(){
+		
+	}
+	
+	public void turnOn(){
+		
+		//default port number
+		int portNumber = 3333;
+		
+		//open server socket on portNumber 
+		try{
+			serverSocket = new ServerSocket(portNumber);
+		}catch(IOException e){
+			System.out.println(e);
+		}
+		
+		//create a client socket for each connection and pass it to a new clinet thread
+		while (true){
+			try{
+				//accept each client
+				clientSocket = serverSocket.accept();
+				int i=0;
+				for (i =0; i< maxClientsCount; i++){
+					if(threads[i] == null){
+						//find free place in array and create client's thread
+						(threads[i] = new ClientThread(clientSocket, threads, this)).start();
+					}
+				}
+				//if there are no free place in array, so there are enought clients 
+				if(i == maxClientsCount){
+					PrintStream os = new PrintStream(clientSocket.getOutputStream());
+					os.println("Server too busy. Try later.");
+					os.close();
+					clientSocket.close();
+				}
+			}catch(IOException e){
+				System.out.println(e);
+			}
+		}
+	}
+}
+
+//class for client's thread
+class ClientThread extends Thread{
+	
+	private Socket clientSocket = null;
+	private ClientThread[] threads;
+	private int maxClientCount;
+	MultiServer server;
+	
+	private DataInputStream is = null;
+	private PrintStream os = null;
+	
+	private String nameFile;
+	private String path;
+	
+	public ClientThread(Socket clientSocket, ClientThread[] threads, MultiServer server){
+		this.clientSocket = clientSocket;
+		this.threads = threads;
+		maxClientCount = threads.length;
+		this.server = server;
+	}
+	
+	
+	public void run(){
+		try{
+			//create input and output streams for this clinet
+			is = new DataInputStream(clientSocket.getInputStream());
+			os = new PrintStream(clientSocket.getOutputStream());
+			os.println("Tutaj wyswietle Ci informacje o plkikach do pobrania, podaj nazwe pliku i sciezke");
+			
+			//read answer from client
+			 String file = is.readLine().trim();
+			 this.nameFile = file;
+			 String where = is.readLine().trim();
+			 this.path = where;
+			 
+			 //server answer
+			 os.println("You've choosen "+ nameFile + " and place for file " + path);
+			 os.println("If you whant end connection enter /guit in a new line");
+			 
+			 while(true){
+				 String line = is.readLine();
+				 
+				 //i work until sb write /guit
+				 
+				 if(line.startsWith("/guit")){
+					 break;
+				 }
+				// if(line.startsWith(prefix))
+			 }
+			 os.println("*** Cliet leave the connection");
+			 
+			 endThread();
+			 
+			 is.close();
+			 os.close();
+			 clientSocket.close();
+		}catch(IOException e){
+			
+		}
+		
+	}
+
+
+	private void endThread() {
+		synchronized(this){
+			for(int i =0; i< maxClientCount; i++){
+				if(threads[i] == this){
+					threads[i] = null;
+				}
+			}
+		}
+	}
+}
